ee3d286e7849cac97b6b09ede915bf410c7fa608
Aaron Jackson <aaron.jcksn@outlook.com>
2021-12-07 14:53:47 -0600

    changed around some JS and form code to try and get the forms to work

--- src/main.js
+++ src/main.js
@@ -37,18 +37,20 @@ export default function (Vue, { router, head, isClient }) {
   })
 }
 
+document.ready(function() {
+  document.querySelector("form").addEventListener("submit", handleSubmit);
 
-document.querySelector("form").addEventListener("submit", handleSubmit);
+  const handleSubmit = (e) => { 
+    e.preventDefault()
+    let myForm = document.getElementById('contact-form');
+    let formData = new FormData(myForm)
+    fetch('/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString()
+    }).then(() => console.log('Form successfully submitted')).catch((error) =>
+      alert(error))
+  }
+})
 
-const handleSubmit = (e) => {
-  e.preventDefault()
-  let myForm = document.getElementById('contact-form');
-  let formData = new FormData(myForm)
-  fetch('/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
-  }).then(() => console.log('Form successfully submitted')).catch((error) =>
-    alert(error))
-}
 
